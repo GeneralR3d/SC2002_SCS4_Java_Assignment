52d684f5bb2f050df884c5831850816279a874e5
Daryl Loh <lohxianjun@gmail.com>
2023-11-21 01:12:15 +0800

    Change methods to static

--- Controllers/DataController.java
+++ Controllers/DataController.java
@@ -3,57 +3,89 @@ import java.util.ArrayList;
 import Entity.*;
 
 public class DataController {
-  private ArrayList<Camp> camps;
-  private ArrayList<Student> students;
-  private ArrayList<Staff> staffs;
-  private ArrayList<CommitteeMember> commMembers;
+  private static ArrayList<Camp> camps;
+  private static ArrayList<Student> students;
+  private static ArrayList<Staff> staffs;
+  private static ArrayList<CommitteeMember> commMembers;
 
-  public void addCamp(Camp camp) {
+  public static ArrayList<Camp> getCamps() {
+    return camps;
+  }
+
+  public static ArrayList<Student> getStudents() {
+    return students;
+  }
+
+  public static ArrayList<Staff> getStaffs() {
+    return staffs;
+  }
+
+  public static ArrayList<CommitteeMember> getCommMembers() {
+    return commMembers;
+  }
+
+  public static void addCamp(Camp camp) {
     camps.add(camp);
   }
 
-  public void addStudent(Student student) {
+  public static void addStudent(Student student) {
     students.add(student);
   }
 
-  public void addStaff(Staff staff) {
+  public static void addStaff(Staff staff) {
     staffs.add(staff);
   }
 
-  public void addCommMember(CommitteeMember commMember) {
+  public static void addCommMember(CommitteeMember commMember) {
     commMembers.add(commMember);
   }
 
-  public void removeCamp(Camp camp) {
+  public static void removeCamp(Camp camp) {
     camps.remove(camp);
   }
 
-  public void removeStudent(Student student) {
+  public static void removeStudent(Student student) {
     students.remove(student);
   }
 
-  public void removeStaff(Staff staff) {
+  public static void removeStaff(Staff staff) {
     staffs.remove(staff);
   }
 
-  public void removeCommMember(CommitteeMember commMember) {
+  public static void removeCommMember(CommitteeMember commMember) {
     commMembers.remove(commMember);
   }
 
-  public Camp findCamp(int campID) {
-    return camps.get(campID);
+  public static Camp findCamp(int campID) {
+    for (int i = 0; i < camps.size(); i++) {
+      Camp camp = camps.get(i);
+      if (campID == camp.getCampID()) return camp;
+    }
+    return null;
   }
 
-  public Student findStudent(int userID) {
-    return students.get(userID);
+  public static Student findStudent(String userID) {
+    for (int i = 0; i < students.size(); i++) {
+      Student student = students.get(i);
+      if (userID.matches(student.getUserID())) return student;
+    }
+    return null;
   }
 
-  public Staff findStaff(int userID) {
-    return staffs.get(userID);
+  public static Staff findStaff(String userID) {
+    for (int i = 0; i < students.size(); i++) {
+      Staff staff = staffs.get(i);
+      if (userID.matches(staff.getUserID())) return staff;
+    }
+    return null;
   }
 
-  public CommitteeMember findCommMember(int userID) {
-    return commMembers.get(userID);
+  public static CommitteeMember findCommMember(String userID) {
+    for (int i = 0; i < students.size(); i++) {
+      CommitteeMember commMember = commMembers.get(i);
+      if (userID.matches(commMember.getUserID())) return commMember;
+    }
+    return null;
   }
 
 }
