2c9c25324c189cd200cca6f00cdc7e24f3b44292
Cragios <35280265+Cragios@users.noreply.github.com>
2023-11-20 23:04:27 +0800

    updated UserController, created SessionInfo

--- Controllers/UserController.java
+++ Controllers/UserController.java
@@ -1,21 +1,28 @@
 package Controllers;
 
 import java.util.ArrayList;
+
+import Entity.SessionInfo;
 import Entity.User;
 
 public class UserController {
-    public static boolean login(ArrayList<User> users, String username, String password){
-        for (User user: users){
-            if (user.signIn(username, password)) return true;
+    public static boolean login(String userID, String password) {
+        // TODO: initialize users array with correct data
+        System.out.println("TODO: initialize users array with correct data");
+        ArrayList<User> users = new ArrayList<User>();
+        for (User user : users) {
+            if (userID == user.getUserID())
+                return true;
         }
         return false;
     }
 
-    public static void resetPassword(ArrayList<User> users, User currUser, String newPW){
-        for (User user: users){
-            if (user.getUserID() == currUser.getUserID()){
-                user.changePw(newPW);
-            }
-        }
+    public static void logout() {
+        SessionInfo.user = null;
+    }
+
+    public static void changePassword(String newPassword) {
+        // TODO: DataController.changePassword(newPassword);
+        System.out.println("TODO: write new password to DataController");
     }
 }
