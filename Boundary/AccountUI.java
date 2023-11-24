2a0cb89a03d235051e60d8213e6125ed7c0f09e4
Daryl Loh <lohxianjun@gmail.com>
2023-11-25 01:05:09 +0800

    22% completed

--- Boundary/AccountUI.java
+++ Boundary/AccountUI.java
@@ -8,6 +8,7 @@ import entity.*;
 public class AccountUI {
     public static void loginMenu() {
         while (true) {
+            System.out.println();
             System.out.println("Camp Application and Management System (CAMS) Login");
             System.out.println("Please enter your username:");
             String username = InputHandler.nextLine();
@@ -21,6 +22,12 @@ public class AccountUI {
                 continue;
             }
             System.out.println();
+            if (password.equals("password")) {
+                System.out.println(
+                        "Welcome to the CAMs! As this is your first time logging in, please change your password.");
+                changePasswordMenu();
+                continue;
+            }
             switch (SessionInfo.getUserType()) {
                 case "Staff":
                     StaffUI.displayMenu();
@@ -36,23 +43,34 @@ public class AccountUI {
         System.out.println("Change Password");
 
         String newPW;
-        do {
+        while (true) {
+            System.out.println();
             System.out.println("Enter your new password:");
             newPW = InputHandler.nextLine();
             System.out.println("Enter your new password again:");
             String newPW2 = InputHandler.nextLine();
-            if (newPW.equals(newPW2))
-                break;
-            else
-                System.out.println("Passwords do not match! Try again.");
-        } while (true);
+            if (!newPW.equals(newPW2)) {
+                System.out.println("Passwords do not match!");
+                continue;
+            }
+            if (newPW.equals(SessionInfo.getUser().getPassword())) {
+                System.out.println("New password cannot be your old password!");
+                continue;
+            }
+            if (newPW.equals("password")) {
+                System.out.println("New password cannot be the default password!");
+                continue;
+            }
+            break;
+        }
 
         UserController.changePassword(newPW);
-        System.out.println("Password reset successfully!");
+        System.out.println("Password changed successfully!");
+        logout();
     }
 
     public static void logout() {
         UserController.logout();
-        System.out.println("User logged out successfully!");
+        System.out.println("You have been logged out.");
     }
 }
