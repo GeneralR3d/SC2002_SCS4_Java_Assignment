eeb02f51b5225315590f280d64be4cba143884ba
Daryl Loh <lohxianjun@gmail.com>
2023-11-25 17:24:47 +0800

    Remove user from input in SuggestionController

--- control/SuggestionController.java
+++ control/SuggestionController.java
@@ -1,5 +1,6 @@
 package control;
 
+import app.SessionInfo;
 import entity.Camp;
 import entity.CommitteeMember;
 import entity.Suggestion;
@@ -12,28 +13,29 @@ public class SuggestionController {
    * @param camp
    * @param content
    */
-  public static void post(User user, Camp camp, String content) {
+  public static void post(Camp camp, String content) {
+    User user = SessionInfo.getUser();
     CommitteeMember commMember = (CommitteeMember) user;
     Suggestion suggestion = new Suggestion(commMember, content);
     camp.addSuggestion(suggestion);
     commMember.addPoint();
   }
 
-  public static void delete(User user, Camp camp, Suggestion suggestion) throws Exception {
+  public static void delete(Camp camp, Suggestion suggestion) throws Exception {
     if (suggestion.isProcessed()) {
       throw new Exception("Suggestion has already been processed!");
     }
     camp.removeSuggestion(suggestion);
   }
 
-  public static void edit(User user, Suggestion suggestion, String content) throws Exception {
+  public static void edit(Suggestion suggestion, String content) throws Exception {
     if (suggestion.isProcessed()) {
       throw new Exception("Suggestion has already been processed!");
     }
     suggestion.edit(content);
   }
 
-  public static void approve(User user, Camp camp, Suggestion suggestion) {
+  public static void approve(Camp camp, Suggestion suggestion) {
     suggestion.approve();
     suggestion.getOwner().addPoint();
   }
