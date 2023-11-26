d9820487b00e94eeae787a0baf954cc66250bc21
Daryl Loh <lohxianjun@gmail.com>
2023-11-26 12:57:12 +0800

    Remove TODO

--- Boundary/AccountUI.java
+++ Boundary/AccountUI.java
@@ -21,24 +21,24 @@ public class AccountUI {
                 System.out.println("Wrong username or password! Please try again");
                 continue;
             }
-            // TODO: check for default password
             if (password.equals("password")) {
                 System.out.println();
-                System.out.println("Welcome to the CAMS! As this is your first time logging in, please change your password.");
+                System.out.println(
+                        "Welcome to the CAMS! As this is your first time logging in, please change your password.");
                 changePasswordMenu();
                 continue;
             }
             switch (SessionInfo.getUserType()) {
-            case "Staff":
-                StaffUI.displayMenu();
-                break;
-            case "CommitteeMember":
-            case "Student":
-                StudentUI.displayMenu();
-                break;
-            default:
-                System.out.println("Invalid user type");
-                break;
+                case "Staff":
+                    StaffUI.displayMenu();
+                    break;
+                case "CommitteeMember":
+                case "Student":
+                    StudentUI.displayMenu();
+                    break;
+                default:
+                    System.out.println("Invalid user type");
+                    break;
             }
         }
     }
